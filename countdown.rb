756b36fa0aafaea83b8d497d4263b7df882905ec
Mike <mclancy96@gmail.com>
2025-07-15 20:19:12 -0400

    Implement countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown starting_int
+  while starting_int > 0
+    puts "#{starting_int} SECOND(S)!"
+    starting_int -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep starting_int
+  while starting_int > 0
+    puts "#{starting_int} SECOND(S)!"
+    starting_int -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
